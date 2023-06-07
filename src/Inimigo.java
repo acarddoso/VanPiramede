46cccff21768640459ea6a6e81404b525c952d9d
ViniciusPetersen <112730065+ViniciusPetersen@users.noreply.github.com>
2023-06-07 20:39:09 -0300

    Update Inimigo.java
    adição de diversas funcionalidades.

--- src/Inimigo.java
+++ src/Inimigo.java
@@ -1,25 +1,30 @@
+import javax.swing.JOptionPane;
+
 public class Inimigo extends ElementoBasico {
     Personagem combate;
-    int vivo=1;
+
 
     public Inimigo(String id, int nroPista, int linInicial, int colInicial, Tabuleiro tabuleiro, Personagem combate) {
         super(id, "hint.jpg", linInicial, colInicial, tabuleiro);
         this.combate = combate;
     }
 
-    public int getVivo(){
-        return vivo;
-    }
-
     @Override
     public void acao(ElementoBasico outro) {
-        if(combate.getVida()>20){
-          combate.dano();
-          vivo = 0;
+        System.out.println(this.toString());
+        if(combate.dano()==true){
+            trocaParaFundo();
         }
+        else {JOptionPane.showMessageDialog(getRootPane(), "Morreu troxa");
+        System.exit(0);}
     
-        throw new UnsupportedOperationException("Unimplemented method 'acao'");
-    }
+    
+       // throw new UnsupportedOperationException("Unimplemented method 'acao'");
+}
 
+    public String toString() {
+        return super.toString() + " combate... vidas: " + combate.getVida();
+    }
     
 }
+
