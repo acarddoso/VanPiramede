427f8819d25c092c7fc9537e49357dee4d81dd73
ViniciusPetersen <112730065+ViniciusPetersen@users.noreply.github.com>
2023-06-17 16:21:24 -0300

    Update ParedeQuebravel.java

--- src/ParedeQuebravel.java
+++ src/ParedeQuebravel.java
@@ -1,12 +1,25 @@
 public class ParedeQuebravel extends ElementoBasico {
+    Personagem parede;
+    boolean danificado = false; 
 
-
-    public ParedeQuebravel(String id, int linInicial, int colInicial, Tabuleiro tabuleiro) {
-        super(id, "wall.jpg", linInicial, colInicial, tabuleiro);
+    public ParedeQuebravel(String id, int linInicial, int colInicial, Tabuleiro tabuleiro, Personagem parede) {
+        super(id, "portal.png", linInicial, colInicial, tabuleiro);
+        this.parede = parede;
     }
 
     @Override
     public void acao(ElementoBasico outro) {
+        System.out.println(this.toString());
+        if(danificado == true){
+            trocaParaFundo();
+        }
+        if(parede.quebrar()==true){
+            danificarparede();
+            danificado = true;
+        }
+    }
 
+    public String toString() {
+        return super.toString();
     }
 }
