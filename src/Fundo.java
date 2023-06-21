a6ec287d2f88e8339697990a8f8c1d6c8fc3e32e
ViniciusPetersen <112730065+ViniciusPetersen@users.noreply.github.com>
2023-06-21 16:00:39 -0300

    Update Fundo.java

--- src/Fundo.java
+++ src/Fundo.java
@@ -1,7 +1,7 @@
 public class Fundo extends ElementoBasico{
 
     public Fundo(String id, int linInicial, int colInicial, Tabuleiro tabuleiro) {
-        super(id, "grass.jpg", linInicial, colInicial, tabuleiro);
+        super(id, "areia.jpg", linInicial, colInicial, tabuleiro);
         //super(id, "fundo.jpg", linInicial, colInicial, tabuleiro);
     }
 
