9781bdb4541884238dd2ba133485bb5fabf4c3ac
ViniciusPetersen <112730065+ViniciusPetersen@users.noreply.github.com>
2023-06-21 15:53:17 -0300

    Update Personagem.java

--- src/Personagem.java
+++ src/Personagem.java
@@ -1,25 +1,127 @@
-public class ParedeQuebravel extends ElementoBasico {
-    Personagem parede;
-    boolean danificado = false; 
+import javax.swing.JOptionPane;
 
-    public ParedeQuebravel(String id, int linInicial, int colInicial, Tabuleiro tabuleiro, Personagem parede) {
-        super(id, "paredequebravel.png", linInicial, colInicial, tabuleiro);
-        this.parede = parede;
+public class Personagem extends ElementoBasico {
+    private ElementoBasico anterior;
+    private boolean machado = false;
+    private static int vida = 50;
+
+    public Personagem(String id, String iconPath, int linInicial, int colInicial, Tabuleiro tabuleiro) {
+        super(id, iconPath, linInicial, colInicial, tabuleiro);
+    }
+
+    public void setAnterior(ElementoBasico anterior){
+        this.anterior = anterior;
+    }
+
+    public boolean quebrar(){
+        if(machado == true){
+            return true;
+        }
+        else return false;
+
+    }
+
+    public void pegaMachado(){
+        machado = true;
+        pegarPicareta();
+        JOptionPane.showMessageDialog(getRootPane(), "Você coletou a picareta, agora você pode quebrar paredes rachadas");
+    }
+
+
+
+
+    public boolean dano(){
+        if(vida>20){
+        vida = vida-20;
+        return true;
+        }
+        else {vida = 0; 
+        return false;}
+    }
+
+    public void cura(){
+        if(vida<40){
+        vida = vida+10;}
+        else vida = 50;
+    }
+
+    public int getVida(){
+        return vida;
+    }
+
+    public ElementoBasico getAnterior() {
+        return anterior;
+    }
+    
+    public void moveDireita() {
+        // Remove o Personagem da posicao atual e avança
+        getTabuleiro().insereElemento(anterior);
+        this.incCol();
+        // Verifica se tem algum elemento de interesse na nova posicao
+        // e interage de acordo
+        ElementoBasico elemento = getTabuleiro().getElementoNaPosicao(this.getLin(), this.getCol());
+        if (!(elemento instanceof Fundo)) {
+            elemento.acao(this);
+            this.decCol();
+            this.anterior = getTabuleiro().insereElemento(this);
+        } else {
+            this.anterior = getTabuleiro().insereElemento(this);
+        }
+    }
+
+    public void moveEsquerda() {
+        // Remove o Personagem da posicao atual e avança
+        getTabuleiro().insereElemento(anterior);
+        this.decCol();
+        // Verifica se tem algum elemento de interesse na nova posicao
+        // e interage de acordo
+        ElementoBasico elemento = getTabuleiro().getElementoNaPosicao(this.getLin(), this.getCol());
+        if (!(elemento instanceof Fundo)) {
+            elemento.acao(this);
+            this.incCol();
+            this.anterior = getTabuleiro().insereElemento(this);
+        } else {
+            this.anterior = getTabuleiro().insereElemento(this);
+        }
+    }
+
+    public void moveCima() {
+        // Remove o Personagem da posicao atual e avança
+        getTabuleiro().insereElemento(anterior);
+        this.decLin();
+        // Verifica se tem algum elemento de interesse na nova posicao
+        // e interage de acordo
+        ElementoBasico elemento = getTabuleiro().getElementoNaPosicao(this.getLin(), this.getCol());
+        if (!(elemento instanceof Fundo)) {
+            elemento.acao(this);
+            this.incLin();
+            this.anterior = getTabuleiro().insereElemento(this);
+        } else {
+            this.anterior = getTabuleiro().insereElemento(this);
+        }
+    }
+
+    public void moveBaixo() {
+        // Remove o Personagem da posicao atual e avança
+        getTabuleiro().insereElemento(anterior);
+        this.incLin();
+        // Verifica se tem algum elemento de interesse na nova posicao
+        // e interage de acordo
+        ElementoBasico elemento = getTabuleiro().getElementoNaPosicao(this.getLin(), this.getCol());
+        if (!(elemento instanceof Fundo)) {
+            elemento.acao(this);
+            this.decLin();
+            this.anterior = getTabuleiro().insereElemento(this);
+        } else {
+            this.anterior = getTabuleiro().insereElemento(this);
+        }
     }
 
     @Override
     public void acao(ElementoBasico outro) {
-        System.out.println(this.toString());
-        if(danificado == true){
-            trocaParaFundo();
-        }
-        if(parede.quebrar()==true){
-            danificarparede();
-            danificado = true;
-        }
+        
+        throw new UnsupportedOperationException("Unimplemented method 'acao'");
     }
 
-    public String toString() {
-        return super.toString();
-    }
+   
 }
